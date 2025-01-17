d6331b8259c15076b39c99021887b2a5ff42f2a4
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-01-17 23:33:05 +0600

    error page added

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,22 @@
+import errorImg from "@/assets/images/error/404.png";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+
 const ErrorPage = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  return (
+    <main>
+      <section className="h-screen flex items-center justify-center">
+        <div className="container text-center">
+          <img src={errorImg} className="inline-block " alt="" />
+          <Button variant="primary" size="lg" onClick={() => navigate("/")}>
+            Back To Home
+          </Button>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default ErrorPage;
